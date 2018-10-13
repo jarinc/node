183679ffabd7fda5d6ee4d81f8811d2bf959d0fe
jarinc <jarinc@gmail.com>
2018-10-13 22:37:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -109,6 +109,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/test', function (req, res) {
+  res.render('test.html', { pageCountMessage : null});
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
